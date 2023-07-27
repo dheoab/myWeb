af3e2b6c0b33b5cc8a3995de42904f0930910186
dheoAb <dheo.abdillah@gmail.com>
2023-07-27 11:43:45 +0700

    portfolio

---
+++ src/content_option.js
@@ -0,0 +1,101 @@
+const logotext = "DHEO";
+const meta = {
+  title: "Dheo Abdillah",
+  description:
+    "I’m Dheo Abdillah _ Fullstack devloper, Hacktiv8 Fullstack Javascript Program Graduates ",
+};
+
+const introdata = {
+  title: "I’m Dheo Abdillah",
+  animated: {
+    first: "I love coding",
+    second: "I code cool websites",
+    third: "I develop mobile apps",
+  },
+  description: `I’m Dheo Abdillah _ Fullstack devloper,\n Hacktiv8 Fullstack Javascript Program Graduates`,
+  your_img_url:
+    "https://images.unsplash.com/photo-1522040806052-b0aa2b039f00?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1064&q=80",
+};
+
+const dataabout = {
+  title: "abit about my self",
+  aboutme:
+    "Results-driven engineer turned tech enthusiast skilled in Node.js, Express, Vue.js, React, and PostgreSQL. Graduated from Hacktiv8 Fullstack Javascript Program .Passionate about innovative tech solutions. Ready for exciting challenges!",
+};
+const worktimeline = [
+  {
+    jobtitle: "Quality Assurance Staff (Non-IT)",
+    where: "PT. Kirana Megatara",
+    date: "2019 - 2022",
+  },
+  {
+    jobtitle: "Quality Control Staff (Non-IT)",
+    where: "PT. Surveyor Indonesia",
+    date: "2017-2018",
+  },
+];
+
+const skills = [
+  {
+    name: "Javascript",
+    value: 80,
+  },
+  {
+    name: "Express JS",
+    value: 75,
+  },
+  {
+    name: "Vue JS",
+    value: 80,
+  },
+  {
+    name: "React",
+    value: 80,
+  },
+];
+
+const services = [];
+
+const dataportfolio = [
+  {
+    img: "https://i.imgur.com/I863Ins.jpg",
+    description: "Real time weather App",
+    link: "https://weatherapp.dheoab.dev/",
+  },
+  {
+    img: "https://i.imgur.com/R7O6F44.png",
+    description:
+      "FlexWell: 30-day home workout challenge, unlock gym exercises, customize routines. Achieve fitness goals!",
+    link: "https://github.com/FlexWell-Team/flexwell-client-mobile",
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "dheo.abdillah@gmail.com",
+  YOUR_FONE: "+62 85717698181",
+  description: "",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  //   YOUR_SERVICE_ID: "service_id",
+  //   YOUR_TEMPLATE_ID: "template_id",
+  //   YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+  github: "https://github.com",
+  //   facebook: "https://facebook.com",
+  linkedin: "https://www.linkedin.com/in/dheo-abdillah/",
+  //   twitter: "https://twitter.com",
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
